b8c067aa3d6c943a232f7b83c2fab9c4858cfa8e
Rodolphe Pineau <pineau@rti-zone.org>
2018-03-31 13:37:52 -0700

    Fix Linux and RPI install script

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -24,14 +24,14 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
-cp "./focuserlist aaf2.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./aaf2.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
-cp "./libaaf2.so" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
+cp "./focuserlist efa.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
+cp "./efa.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
+cp "./libefa.so" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
-	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/focuserlist aaf2.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/aaf2.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libaaf2.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/focuserlist efa.txt"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/efa.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libefa.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libaaf2.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libefa.so"
