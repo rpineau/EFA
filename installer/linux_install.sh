b09828ba6d74a4e5f0d578c3b95e0d4f27b9ed67
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:31:47 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,14 +22,20 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./focuserlist efa.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./efa.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
-cp "./libefa.so" "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/"
+cp "./efa.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
+cp "./libefa.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/focuserlist efa.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/efa.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libefa.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/efa.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libefa.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FocuserPlugIns/libefa.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FocuserPlugIns/libefa.so"
