12277b03cde3343d8bbb66f0395173e82ad15342
Rodolphe Pineau <pineau@rti-zone.org>
2020-01-02 09:21:20 -0800

    fix macOS code signing for 64 bit

--- installer/build.sh
+++ installer/build.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+PACKAGE_NAME="efa_X2.pkg"
+BUNDLE_NAME="org.rti-zone.efaX2"
+
+if [ ! -z "$app_id_signature" ]; then
+    codesign -s "$app_id_signature" ../build/Release/libefa.dylib
+fi
+
 mkdir -p ROOT/tmp/efa_X2/
 cp "../efa.ui" ROOT/tmp/efa_X2/
 cp "../focuserlist efa.txt" ROOT/tmp/efa_X2/
 cp "../build/Release/libefa.dylib" ROOT/tmp/efa_X2/
 
+
 if [ ! -z "$installer_signature" ]; then
-# signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.efa_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 efa_X2.pkg
-pkgutil --check-signature ./efa_X2.pkg
+	# signed package using env variable installer_signature
+	pkgbuild --root ROOT --identifier $BUNDLE_NAME --sign "$installer_signature" --scripts Scripts --version 1.0 $PACKAGE_NAME
+	pkgutil --check-signature ./${PACKAGE_NAME}
+
 else
-pkgbuild --root ROOT --identifier org.rti-zone.efa_X2 --scripts Scripts --version 1.0 efa_X2.pkg
+    pkgbuild --root ROOT --identifier $BUNDLE_NAME --scripts Scripts --version 1.0 $PACKAGE_NAME
 fi
 
 rm -rf ROOT
